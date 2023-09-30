484a1c934b9775afeadffcac5f42a7b66ecd9770
pedrohma07 <pedrohma07@gmail.com>
2023-09-30 19:49:58 -0300

    :sparkles:	feat: added eslint

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { Card } from 'src/app/interface/Card';
+import { Component } from "@angular/core";
+import { Card } from "src/app/interface/Card";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"]
 })
 export class CardComponent {
   cards: Card[] = [
