4290f6567ffb9188a0a8054c9fbb470aca0ba3f8
pedrohma07 <pedrohma07@gmail.com>
2023-10-01 17:24:30 -0300

    :sparkles:	feat: character search input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CardComponent } from "./components/card/card.component";
+import { SearchInputComponent } from './components/search-input/search-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    SearchInputComponent
   ],
   imports: [
     BrowserModule,
